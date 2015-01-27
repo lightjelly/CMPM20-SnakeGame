714d9a0db3e9bbaa10116be1f20b5858ff0f8df9
unknown <swong30@.lt.ucsc.edu>
2015-01-27 15:49:19 -0800

    Food Init

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ var body = new Sprite();
 
 body.width = "20";
 body.height = "20";
-body.x = "400";
-body.y = "400";
-body.image = Textures.load("body.png");
+body.x = '260';
+body.y = '260';
+body.image = Textures.load("image/body.png");
+
+world.addChild(body);
+
+var food = new Sprite();
+//The food spawns at a random location
+food.width = '20';
+food.height = '20';
+food.x = 20*Math.round(20*Math.random());
+food.y = 20*Math.round(20*Math.random());
+food.image = Textures.load("image/food.png");
+
+world.addChild(food);
